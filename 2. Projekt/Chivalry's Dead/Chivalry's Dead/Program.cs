0553015e48110b1fdc22fd8cc4d39472c39a05c2
xXWEEBMASTERXx <74237896+xXWEEBMASTERXx@users.noreply.github.com>
2022-12-14 14:11:59 +0100

    Title screen implemented
    Fuck Menczel

--- 2. Projekt/Chivalry's Dead/Chivalry's Dead/Program.cs
+++ 2. Projekt/Chivalry's Dead/Chivalry's Dead/Program.cs
@@ -6,12 +6,42 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Drawing;
 using System.Windows.Media;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Chivalrys_Dead
 {
-
     class Game
     {
+        static void ConsoleDraw(IEnumerable<string> lines, int x, int y)
+        {
+            if (x > Console.WindowWidth) return;
+            if (y > Console.WindowHeight) return;
+
+            var trimLeft = x < 0 ? -x : 0;
+            int index = y;
+
+            x = x < 0 ? 0 : x;
+            y = y < 0 ? 0 : y;
+
+            var linesToPrint =
+                from line in lines
+                let currentIndex = index++
+                where currentIndex > 0 && currentIndex < Console.WindowHeight
+                select new
+                {
+                    Text = new String(line.Skip(trimLeft).Take(Math.Min(Console.WindowWidth - x, line.Length - trimLeft)).ToArray()),
+                    X = x,
+                    Y = y++
+                };
+
+            Console.Clear();
+            foreach (var line in linesToPrint)
+            {
+                Console.SetCursorPosition(line.X, line.Y);
+                Console.Write(line.Text);
+            }
+        }
         class Sound
         {
             private MediaPlayer music_Player;
@@ -30,6 +60,7 @@ namespace Chivalrys_Dead
         }
         static void Main(string[] args)
         {
+            Random Title = new Random();
             Sound music = new Sound();
             bool IsAllAlphabetic(string value)
             {
@@ -41,43 +72,146 @@ namespace Chivalrys_Dead
 
                 return true;
             }
-            /*
-            FontFamily fontFamily = new FontFamily("Old English Text MT");
-            Font font = new Font(
-            fontFamily,
-            72,
-            FontStyle.Regular,
-            GraphicsUnit.Pixel);
-
-            Console.WriteLine("Chivalry's Dead");
-            */
-
-
-
-
-
-
-
-            Console.WriteLine("Please lower volume before pressing any keys!");
-            Console.ReadKey();
+            // Music Setup
+            Console.WriteLine("Add meg százalékban hogy milyen hangos legyen a játék");
+            int Game_Volume = int.Parse(Console.ReadLine());
+            // Start Main Menu theme
             string Main_Menu_Music = "Searching for Integrity.wav";
-
-            // Name input
             music.Play(Main_Menu_Music);
-            music.SetVolume(5);
-            Console.Write("Enter your name: ");
-            var Temporary_name = Console.ReadLine();
-            if (IsAllAlphabetic(Temporary_name) is true)
+            music.SetVolume(Game_Volume);
+
+
+            var Title01 = new[]
             {
-                string Player_name = Temporary_name;
-    }
-            else
+                    @":::===== :::  === ::: :::  === :::====  :::      :::====  ::: ===  == :::===       :::====  :::===== :::====  :::==== ",
+                    @":::      :::  === ::: :::  === :::  === :::      :::  === ::: === ==  :::          :::  === :::      :::  === :::  ===",
+                    @"===      ======== === ===  === ======== ===      =======   =====       =====       ===  === ======   ======== ===  ===",
+                    @"===      ===  === ===  ======  ===  === ===      === ===    ===           ===      ===  === ===      ===  === ===  ===",
+                    @" ======= ===  === ===    ==    ===  === ======== ===  ===   ===       ======       =======  ======== ===  === ======= ",
+            };
+            var Title02 = new[]
             {
-                Console.WriteLine("Error: name contains non Alphabetic characters");
-                Console.ReadKey();
-                Environment.Exit(0);
-             
+                    @" ▄████▄   ██░ ██  ██▓ ██▒   █▓ ▄▄▄       ██▓     ██▀███ ▓██   ██▓  ██████    ▓█████▄ ▓█████ ▄▄▄      ▓█████▄ ",
+                    @"▒██▀ ▀█  ▓██░ ██▒▓██▒▓██░   █▒▒████▄    ▓██▒    ▓██ ▒ ██▒▒██  ██▒▒██    ▒    ▒██▀ ██▌▓█   ▀▒████▄    ▒██▀ ██▌",
+                    @"▒▓█    ▄ ▒██▀▀██░▒██▒ ▓██  █▒░▒██  ▀█▄  ▒██░    ▓██ ░▄█ ▒ ▒██ ██░░ ▓██▄      ░██   █▌▒███  ▒██  ▀█▄  ░██   █▌",
+                    @"▒▓▓▄ ▄██▒░▓█ ░██ ░██░  ▒██ █░░░██▄▄▄▄██ ▒██░    ▒██▀▀█▄   ░ ▐██▓░  ▒   ██▒   ░▓█▄   ▌▒▓█  ▄░██▄▄▄▄██ ░▓█▄   ▌",
+                    @"▒ ▓███▀ ░░▓█▒░██▓░██░   ▒▀█░   ▓█   ▓██▒░██████▒░██▓ ▒██▒ ░ ██▒▓░▒██████▒▒   ░▒████▓ ░▒████▒▓█   ▓██▒░▒████▓ ",
+                    @"░ ░▒ ▒  ░ ▒ ░░▒░▒░▓     ░ ▐░   ▒▒   ▓▒█░░ ▒░▓  ░░ ▒▓ ░▒▓░  ██▒▒▒ ▒ ▒▓▒ ▒ ░    ▒▒▓  ▒ ░░ ▒░ ░▒▒   ▓▒█░ ▒▒▓  ▒ ",
+                    @"  ░  ▒    ▒ ░▒░ ░ ▒ ░   ░ ░░    ▒   ▒▒ ░░ ░ ▒  ░  ░▒ ░ ▒░▓██ ░▒░ ░ ░▒  ░ ░    ░ ▒  ▒  ░ ░  ░ ▒   ▒▒ ░ ░ ▒  ▒ ",
+                    @"░         ░  ░░ ░ ▒ ░     ░░    ░   ▒     ░ ░     ░░   ░ ▒ ▒ ░░  ░  ░  ░      ░ ░  ░    ░    ░   ▒    ░ ░  ░ ",
+                    @"░ ░       ░  ░  ░ ░        ░        ░  ░    ░  ░   ░     ░ ░           ░        ░       ░  ░     ░  ░   ░    ",
+                    @"░                         ░                              ░ ░                  ░                       ░      ",
+            };
+            var Title03 = new[]
+            {
+                    @" ██████ ██   ██ ██ ██    ██  █████  ██      ██████  ██    ██ ███████     ██████  ███████  █████  ██████  ",
+                    @"██      ██   ██ ██ ██    ██ ██   ██ ██      ██   ██  ██  ██  ██          ██   ██ ██      ██   ██ ██   ██ ",
+                    @"██      ███████ ██ ██    ██ ███████ ██      ██████    ████   ███████     ██   ██ █████   ███████ ██   ██ ",
+                    @"██      ██   ██ ██  ██  ██  ██   ██ ██      ██   ██    ██         ██     ██   ██ ██      ██   ██ ██   ██ ",
+                    @" ██████ ██   ██ ██   ████   ██   ██ ███████ ██   ██    ██    ███████     ██████  ███████ ██   ██ ██████  ",
+            };
+            int Titlenum = Title.Next(1, 4);
+            switch (Titlenum)
+            {
+                case 1:
+                    Console.WindowWidth = 160;
+                    Console.WriteLine("\n\n");
+                    var maxLength = Title01.Aggregate(0, (max, line) => Math.Max(max, line.Length));
+                    var x = Console.BufferWidth / 2 - maxLength / 2;
+                    for (int y = -Title01.Length; y < Console.WindowHeight + Title01.Length; y++)
+                    {
+                        ConsoleDraw(Title01, x, y);
+                        Thread.Sleep(100);
+                    }
+                    break;
+                case 2:
+                    Console.WindowWidth = 160;
+                    Console.WriteLine("\n\n");
+                    var maxLength2 = Title02.Aggregate(0, (max, line) => Math.Max(max, line.Length));
+                    var x2 = Console.BufferWidth / 2 - maxLength2 / 2;
+                    for (int y = -Title02.Length; y < Console.WindowHeight + Title02.Length; y++)
+                    {
+                        ConsoleDraw(Title01, x2, y);
+                        Thread.Sleep(100);
+                    }
+                    break;
+                case 3:
+                    Console.WindowWidth = 160;
+                    Console.WriteLine("\n\n");
+                    var maxLength3 = Title03.Aggregate(0, (max, line) => Math.Max(max, line.Length));
+                    var x3 = Console.BufferWidth / 2 - maxLength3 / 2;
+                    for (int y = -Title03.Length; y < Console.WindowHeight + Title03.Length; y++)
+                    {
+                        ConsoleDraw(Title03, x3, y);
+                        Thread.Sleep(100);
+                    }
+                    break;
             }
+            
+            // Name input
+            Console.Write("Your name (2-20 characters): ");
+            var Temporary_name = Console.ReadLine();
+            string Player_name = "";
+            int Max_name_length = 20;
+            while (true)
+            {
+                if (IsAllAlphabetic(Temporary_name) is true)
+                {
+                    if (Temporary_name.Length <= Max_name_length)
+                    {
+                        Player_name = Temporary_name;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Name is too long, use 2-20 characters.");
+                        Console.ReadKey();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error: Name contains non-alphabetic characters.");
+                    Console.ReadKey();
+                }
+
+                Console.Write($"Are you {Player_name}? (yes/no) ");
+                string Answer = Console.ReadLine();
+                while (Answer != "Yes" || Answer != "yes")
+                {
+                    if (Answer == "yes" || Answer == "Yes")
+                    {
+                        Console.WriteLine("Press a key to start");
+                        Console.ReadKey();
+                        break;
+                    }
+                    else if (Answer == "no" || Answer == "No")
+                    {
+                        Console.Write("Your name (2-20 characters): ");
+                        Temporary_name = Console.ReadLine();
+                        if (IsAllAlphabetic(Temporary_name) is true)
+                        {
+                            if (Temporary_name.Length <= Max_name_length)
+                            {
+                                Player_name = Temporary_name;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error: Name is too long, use 2-20 characters.");
+                                Console.ReadKey();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Name contains non-alphabetic characters.");
+                            Console.ReadKey();
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            //Console.ReadKey();
         }
     }
 }
